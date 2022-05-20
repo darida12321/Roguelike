1291a83ffe96e7d0e0bb71c970d076f899a2e2f8
Dániel Mihalik <dani.mihalik@gmail.com>
2022-05-21 00:02:21 +0100

    break test to test CI/CD

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -3,6 +3,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 class CubeCalculatorTest extends AnyFlatSpec {
   "CubeCalculator.cube" should "calculate cubes" in {
 
-    assert(CubeCalculator.cube(3) === 27)
+    assert(CubeCalculator.cube(3) === 28)
   }
 }
