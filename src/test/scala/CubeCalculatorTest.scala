656f7eb6727ab237cb973330a96f4dcea877cf67
Dániel Mihalik <dani.mihalik@gmail.com>
2022-05-21 00:04:49 +0100

    fix test to check CI/CD

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -2,7 +2,6 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class CubeCalculatorTest extends AnyFlatSpec {
   "CubeCalculator.cube" should "calculate cubes" in {
-
-    assert(CubeCalculator.cube(3) === 28)
+    assert(CubeCalculator.cube(3) === 27)
   }
 }
