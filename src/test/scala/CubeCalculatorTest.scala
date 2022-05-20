57b3baa7ca51ae235ed0b03cdc4c7a69f975ddd1
Dániel Mihalik <dani.mihalik@gmail.com>
2022-05-20 23:54:50 +0100

    use anyFlatSpec for testing

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -1,5 +1,7 @@
-class CubeCalculatorTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("CubeCalculator.cube") {
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CubeCalculatorTest extends AnyFlatSpec {
+  "CubeCalculator.cube" should "calculate cubes" in {
     assert(CubeCalculator.cube(3) === 27)
   }
 }
