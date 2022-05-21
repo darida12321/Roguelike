3a3cb31028e1981e0d6ac1264b26a1e2b00cfebb
fh720 <fh720@doc.ic.ac.uk>
2022-05-21 22:07:39 +0100

    created skeletal draft of main function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,29 @@
+import scala.io.StdIn.readLine
 
 object Main extends App {
-  val img = new Image(10, 5)
-  img.clear('?')
-  img.setChar(1, 2, 'a')
-  img.setChar(1, 3, 'r', Console.RED)
-  img.display()
+  val map = new Map(10, 10)
+  var pos_x = 0
+  var pos_y = 0
+  map.grid(pos_x)(pos_y) = Full("C")
+  var loopAgain = true
+  while (loopAgain) {
+    println(map.display())
+    var prompt = readLine("> ")
+    prompt match {
+      case "u" => updatePos(-1, 0)
+      case "d" => updatePos(1, 0)
+      case "l" => updatePos(0, -1)
+      case "r" => updatePos(0, 1)
+      case _ => {
+        loopAgain = false
+      }
+    }
+  }
+
+  def updatePos(x_off: Int, y_off: Int): Unit = {
+    map.grid(pos_x + x_off)(pos_y + y_off) = map.grid(pos_x)(pos_y)
+    map.grid(pos_x)(pos_y) = Empty
+    pos_x = pos_x + x_off
+    pos_y = pos_y + y_off
+  }
 }
