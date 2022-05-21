b1952b5715aefdfe0d16256a52a989e2524315a0
fh720 <fh720@doc.ic.ac.uk>
2022-05-22 00:54:13 +0100

    removed duplicate functionality between functions move and warp

--- src/main/scala/Map.scala
+++ src/main/scala/Map.scala
@@ -22,17 +22,6 @@ class TileMap(x: Int, y: Int) {
     }
   }
   
-  // moves entity an offset of (i, j) from their current location
-  // TODO handle preventing movement into tiles that do not exist
-  def move(e: Entity, i: Int, j: Int): Unit = dict.get(e) match {
-    case Some((x: Int, y: Int)) => {
-      grid(x)(y) = Room(grid(x)(y).es - e)
-      dict(e) = (x + i, y + j)
-      grid(x + i)(y + j) = Room(grid(x + i)(y + j).es + e)
-    }
-    case _ =>
-  }
-
   // directly teleports entity e to tile (i, j) on the grid
   // TODO handle preventing warping into tiles that do not exist
   def warp(e: Entity, i: Int, j: Int): Unit = dict.get(e) match {
@@ -41,7 +30,14 @@ class TileMap(x: Int, y: Int) {
       dict(e) = (i, j)
       grid(i)(j) = Room(grid(i)(j).es + e)
     }
-    case _ =>
+    case _ => // do nothing
+  }
+
+  // moves entity an offset of (i, j) from their current location
+  // TODO handle preventing movement into tiles that do not exist
+  def move(e: Entity, i: Int, j: Int): Unit = dict.get(e) match {
+    case Some((x, y)) => warp(e, x + i, y + j)
+    case _ => // do nothing
   }
 
   // TODO outsource this functionality to the generation of an image.
