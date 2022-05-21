6a9a97bc136b1c0bb36faf992c14a5b642851cfa
fh720 <fh720@doc.ic.ac.uk>
2022-05-22 00:55:15 +0100

    changed name of function warp to moveTo

--- src/main/scala/Map.scala
+++ src/main/scala/Map.scala
@@ -24,7 +24,7 @@ class TileMap(x: Int, y: Int) {
   
   // directly teleports entity e to tile (i, j) on the grid
   // TODO handle preventing warping into tiles that do not exist
-  def warp(e: Entity, i: Int, j: Int): Unit = dict.get(e) match {
+  def moveTo(e: Entity, i: Int, j: Int): Unit = dict.get(e) match {
     case Some((x: Int, y: Int)) => {
       grid(x)(y) = Room(grid(x)(y).es - e)
       dict(e) = (i, j)
@@ -36,7 +36,7 @@ class TileMap(x: Int, y: Int) {
   // moves entity an offset of (i, j) from their current location
   // TODO handle preventing movement into tiles that do not exist
   def move(e: Entity, i: Int, j: Int): Unit = dict.get(e) match {
-    case Some((x, y)) => warp(e, x + i, y + j)
+    case Some((x, y)) => moveTo(e, x + i, y + j)
     case _ => // do nothing
   }
 
