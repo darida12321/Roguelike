2f26068beac044c9cc3d16091ef5513ab7233454
fh720 <fh720@doc.ic.ac.uk>
2022-05-22 01:03:28 +0100

    removed compilation errors (god i am so stupid)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import scala.io.StdIn.readLine
 object Main extends App {
   val map = new TileMap(10, 10)
   val player = new Entity()
-  map.place(player, pos_x, pos_y)
+  map.place(player, 0, 0)
   var loopAgain = true
   while (loopAgain) {
     // TODO offload this shit to a parser
@@ -12,8 +12,8 @@ object Main extends App {
     prompt match {
       case "u" => map.move(player, -1, 0)
       case "d" => map.move(player, 1, 0)
-      case "l" => map.move(player, pos_x, pos_y - 1)
-      case "r" => map.move(player, pos_x, pos_y + 1)
+      case "l" => map.move(player, 0, -1)
+      case "r" => map.move(player, 0, 1)
       case "q" => {
         loopAgain = false
       }
