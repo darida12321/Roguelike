57eed9ff547ab66fd66fba1d6f562d70f546baef
fh720 <fh720@doc.ic.ac.uk>
2022-05-22 09:29:00 +0100

    created base implementation of Room class

--- src/main/scala/Room.scala
+++ src/main/scala/Room.scala
@@ -1,13 +1,20 @@
 import scala.collection.mutable
 import scala.collection.Set
 
-class RoomMap {
+object RoomMap {
   private var contents = Set.empty[Entity]
-  private var rooms = Set.empty[Room]
-
+  var rooms = Set.empty[Room]
 }
 
 class Room(es: Set[Entity]) {
   private var contents = es
-  private var up: Option[Room] = 
+  private val connections = Array.ofDim[Option[Room]](4)
+  for (i <- 0 until 4) {
+    connections(i) = None
+  }
+
+  def up(): Option[Room] = connections(0)
+  def down(): Option[Room] = connections(1)
+  def left(): Option[Room] = connections(2)
+  def right(): Option[Room] = connections(3)
 }
