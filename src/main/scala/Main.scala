f2955dc9c6b2cecacd7aed4af889c57906f61cc5
Dániel Mihalik <dani.mihalik@gmail.com>
2022-05-25 13:42:48 +0100

    Add todo instructions to the main method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,9 @@ import scala.io.StdIn.readLine
 
 object Main extends App {
   val e1 = new Robot();
-
+  
+  // TODO Add room index
+  // TODO Add tests
   val roomMap = MapGenerator.getPredefinedMap()
   roomMap.rooms.head.addEntity(e1)
   
