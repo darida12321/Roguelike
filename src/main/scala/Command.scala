e3982f3cf20851d47235fc2cf1bbcd0ef6cae44e
fh720 <fh720@doc.ic.ac.uk>
2022-05-25 23:03:11 +0100

    added stub for new command structure

--- src/main/scala/Command.scala
+++ src/main/scala/Command.scala
@@ -1,6 +1,12 @@
+import scala.collection.Map
+
 sealed trait Command
 class Move(d: Direction) extends Command
 class Scan(d: Direction) extends Command
 object Mine extends Command
 class Attack(e: Entity) extends Command // TODO consider weapons?
 // TODO add identifiers to map commands to entities
+
+object Commands {
+  val COMMANDS: Map[String, ((Entity, List[Any]) => Unit, List[ (String) => Any])] = Map.empty()
+}
