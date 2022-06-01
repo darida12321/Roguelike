835c62da83625bf71f73318ceeaa51fe9dd53263
Dániel Mihalik <dani.mihalik@gmail.com>
2022-06-01 12:51:26 +0100

    Add tests for adding and removing entities from rooms

--- src/test/scala/RoomTest.scala
+++ src/test/scala/RoomTest.scala
@@ -2,20 +2,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
 class RoomTest extends AnyFlatSpec {
-  val room = new Room(1, 1, 1, Set.empty)
-  val room1 = new Room(1, 1, 2, Set.empty)
-  val room2 = new Room(1, 1, 3, Set.empty)
-  val room3 = new Room(1, 1, 4, Set.empty)
-  val room4 = new Room(1, 1, 5, Set.empty)
 
   behavior of "Room connections"
   it should "initialize with no connections" in {
+    val room = new Room(1, 1, 1, Set.empty)
     room.roomAt(Right) shouldBe None
     room.roomAt(Up) shouldBe None
     room.roomAt(Left) shouldBe None
     room.roomAt(Down) shouldBe None
   }
   it should "add new connections in different directions" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    val room1 = new Room(1, 1, 2, Set.empty)
+    val room2 = new Room(1, 1, 3, Set.empty)
+    val room3 = new Room(1, 1, 4, Set.empty)
+    val room4 = new Room(1, 1, 5, Set.empty)
+    
     room.connect(Right, room1)
     room.connect(Up, room2)
     room.connect(Left, room3)
@@ -27,6 +29,12 @@ class RoomTest extends AnyFlatSpec {
     room.roomAt(Down) shouldBe Some(room4)
   }
   it should "make the other room reconnect in correct direction" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    val room1 = new Room(1, 1, 2, Set.empty)
+    val room2 = new Room(1, 1, 3, Set.empty)
+    val room3 = new Room(1, 1, 4, Set.empty)
+    val room4 = new Room(1, 1, 5, Set.empty)
+
     room.connect(Right, room1)
     room.connect(Up, room2)
     room.connect(Left, room3)
@@ -38,12 +46,52 @@ class RoomTest extends AnyFlatSpec {
     room4.roomAt(Up) shouldBe Some(room)
   }
 
+  class TestEntity(val char: Char, val visible: Boolean) extends Entity {
+    val colour = Console.RESET
+  }
+
+  behavior of "Room entities"
+  it should "start with no entities" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    room.getEntities().size shouldBe 0
+  }
+  it should "keep track of added entities" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    val e1 = new TestEntity('a', true)
+    val e2 = new TestEntity('b', true)
+    room.addEntity(e1)
+    room.getEntities() shouldBe Set(e1)
+    room.addEntity(e2)
+    room.getEntities() shouldBe Set(e1, e2)
+  }
+  it should "throw an error when adding an entity twice" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    val e1 = new TestEntity('a', true)
+    room.addEntity(e1)
+    room.getEntities() shouldBe Set(e1)
+    an [IllegalArgumentException] should be thrownBy room.addEntity(e1)
+  }
+  it should "remove entities" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    val e1 = new TestEntity('a', true)
+    val e2 = new TestEntity('b', true)
+    room.addEntity(e1)
+    room.addEntity(e2)
+    room.getEntities() shouldBe Set(e1, e2)
+    room.removeEntity(e1)
+    room.getEntities() shouldBe Set(e2)
+    room.removeEntity(e2)
+    room.getEntities() shouldBe Set()
+  }
+  it should "throw an error when removing non-existent entities" in {
+    val room = new Room(1, 1, 1, Set.empty)
+    val e1 = new TestEntity('a', true)
+    an [IllegalArgumentException] should be thrownBy room.removeEntity(e1)
+  }
 
 
 }
 
-// roomAt
-// connect
 // addEntity
 // removeEntity
 // containsEntity
